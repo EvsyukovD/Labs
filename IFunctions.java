b92ea785c5494bcbd7ce7231b5b605dd01fd24dd
Gibson <70591237+EvsyukovD@users.noreply.github.com>
2021-09-25 16:36:12 +0300

    Add files via upload

---
+++ IFunctions.java
@@ -0,0 +1,15 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public interface IFunctions {
+      String readLine(Scanner scan);
+      boolean checkLength(String s);
+      void processString(String s);
+      String sort(String s);
+      boolean isDigit(char c);
+      void setFlag(String s);
+      boolean getFlag();
+      double getNum(String s,int startIdx,int[] endIdx);
+      void swap(StringBuilder s,int i,int j);
+}
